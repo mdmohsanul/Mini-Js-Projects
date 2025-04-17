0eba7e62a02e851d979e30bb383e0a296d8d75a3
Mohsanul786 <mdmohsan2407@gmail.com>
2025-04-17 21:13:10 +0530

    projects updated

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,6 +1,37 @@
-var time = new Date();
+const currTime = document.querySelector("#time");
+const currLocation = document.querySelector("#location");
+
+let latitude;
+let longitude;
+const successCallback = (position) => {
+  console.log(position);
+  console.log(position.coords.latitude);
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+};
+
+console.log(latitude);
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+var time = new Date().toLocaleTimeString();
 console.log(time);
 
-const currTime = document.querySelector("#time");
-
 currTime.innerHTML = time;
+
+async function getCityName() {
+  let res = await fetch(
+    "http://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+      latitude +
+      "," +
+      longitude +
+      "&sensor=true"
+  );
+  let data = await res.json();
+  return data;
+}
+
+console.log(getCityName());
